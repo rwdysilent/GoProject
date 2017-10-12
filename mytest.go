ba0a2562b021c518662a2995c0c4e2751a516695
wupengfei <wupengfei@sogou-inc.com>
2017-10-13 00:17:23 +0800

    start learn go

--- mytest.go
+++ mytest.go
@@ -4,25 +4,26 @@ import "fmt"
 
 func Append(slice, data[]byte) []byte {
 	l := len(slice)
-	if l + len(data) > cap(slice) {  // 重新分配
+	if l + len(data) > cap(slice) {
+		// 重新分配
 		// 为了后面的增长，需分配两份。
-		newSlice := make([]byte, (l+len(data))*2)
+		newSlice := make([]byte, (l + len(data)) * 2)
 		// copy 函数是预声明的，且可用于任何切片类型。
 		copy(newSlice, slice)
 		slice = newSlice
 	}
-	slice = slice[0:l+len(data)]
+	slice = slice[0:l + len(data)]
 	for i, c := range data {
-		slice[l+i] = c
+		slice[l + i] = c
 	}
 	return slice
 }
 
-func main(){
+func main_back() {
 	s := make([]byte, 0, 5)
 	fmt.Println(len(s), cap(s))
 	s = append(s, 1, 2, 3, 4, 5, 6)
-	d := []byte{1,2,3,4,5,6}
+	d := []byte{1, 2, 3, 4, 5, 6}
 	fmt.Println(len(s), cap(s))
 	var mews[]byte
 	mews = Append(s, d)
@@ -30,4 +31,14 @@ func main(){
 
 	t := s[0:1]
 	fmt.Println(t, s)
+}
+
+func main() {
+	type LinesOfText [][]byte
+	text := LinesOfText{
+		[]byte("Now is the time"),
+		[]byte("for all good gophers"),
+		[]byte("to bring some fun to the party."),
+	}
+	fmt.Println(text)
 }
