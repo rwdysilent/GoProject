5389591ccd18ccc4e5188227788de86b887a8f97
wupengfei <wupengfei@sogou-inc.com>
2017-10-12 20:33:05 +0800

    start learn go

--- mytest.go
+++ mytest.go
@@ -1,17 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	b := []int{3, 4}
-	var x int
-	for i := 0; i < len(b); {
-		x, i = nextInt(b, i)
-		fmt.Println(x)
+func Append(slice, data[]byte) []byte {
+	l := len(slice)
+	if l + len(data) > cap(slice) {  // 重新分配
+		// 为了后面的增长，需分配两份。
+		newSlice := make([]byte, (l+len(data))*2)
+		// copy 函数是预声明的，且可用于任何切片类型。
+		copy(newSlice, slice)
+		slice = newSlice
 	}
+	slice = slice[0:l+len(data)]
+	for i, c := range data {
+		slice[l+i] = c
+	}
+	return slice
 }
-func nextInt(ints []int, i int) (int, int) {
 
+func main(){
+	s := make([]byte, 0, 5)
+	fmt.Println(len(s), cap(s))
+	s = append(s, 1, 2, 3, 4, 5, 6)
+	d := []byte{1,2,3,4,5,6}
+	fmt.Println(len(s), cap(s))
+	var mews[]byte
+	mews = Append(s, d)
+	fmt.Println(mews)
+
+	t := s[0:1]
+	fmt.Println(t, s)
 }
